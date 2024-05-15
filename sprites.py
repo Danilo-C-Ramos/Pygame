9cde3a6b16fa0315260d018941f9826db969a2d5
Danilo <danilocr@al.insper.edu.br>
2024-05-15 09:13:43 -0300

    .

--- sprites.py
+++ sprites.py
@@ -29,7 +29,7 @@ class Grama(pygame.sprite.Sprite):
         if state == 1:
             self.image = assets[G_RETA]
         elif state == 2:
-            self.imgage = assets[G_RETA_E]
+            self.image = assets[G_RETA_E]
         elif state == 3:
             self.image = assets[G_RETA_D]
         elif state == 4:
